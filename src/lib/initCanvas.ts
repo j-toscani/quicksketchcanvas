3eb9c0119216bdb63f5f11252364f2cb578dbc56
Julian Toscani <toscani@cekom.de>
2021-07-31 23:18:27 +0200

    init restore rtevert buttons

--- src/lib/initCanvas.ts
+++ src/lib/initCanvas.ts
@@ -8,10 +8,18 @@ export default function initCanvas(query: string): CanvasInfos  {
     const canvas = createCanvas(query);
     const active = canvas.init();
     const available = initDrawableButtons(canvas);
-
+    initRevertRestore(canvas);
     return {
         canvas: canvas,
         active,
         available
     }
+}
+
+function initRevertRestore(canvas: Canvas) {
+    const revert = document.querySelector("#revert");
+    const restore = document.querySelector("#restore");
+    
+    revert?.addEventListener("click", () => canvas.revert());
+    restore?.addEventListener("click", () => canvas.restore());
 }
