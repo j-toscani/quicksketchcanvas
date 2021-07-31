7d177177566bcefc168796b91c26cf8509283ae4
Julian Toscani <toscani@cekom.de>
2021-07-31 16:46:44 +0200

    create types for drawables

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,3 +7,19 @@ type Distance = {
     dx: number,
     dy: number
 }
+
+export abstract class DrawElement<T> {
+    data: T;
+    ctx: RenderingContext2D;
+
+    constructor(ctx: RenderingContext, data: T) {
+        this.ctx = ctx;
+        this.data = data;
+    }
+}
+
+interface Drawable {
+    draw: (position: Coordinates) => void;
+    select: () => void;
+    deselect: () => void;
+}
