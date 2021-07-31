ac0f120060e686d0e7eec7433e9fcea8912a079a
Julian Toscani <toscani@cekom.de>
2021-07-31 23:17:39 +0200

    implement history on canvas and format

--- src/lib/Canvas.ts
+++ src/lib/Canvas.ts
@@ -1,14 +1,17 @@
-import { getDistanceFromTo, getElementPosition } from "./createGetClickCoordinatesOn.js";
+import {
+  getDistanceFromTo,
+  getElementPosition,
+} from "./createGetClickCoordinatesOn.js";
 import Rect from "../drawables/Rect.js";
 import Circle from "../drawables/Circle.js";
 
-export type DrawOptions = {rect: Rect, circle: Circle}
+export type DrawOptions = { rect: Rect; circle: Circle };
 
 function createDrawOptions(canvas: Canvas) {
   return {
-    "rect": new Rect(canvas, {w: 100, h: 100, style: "black"}),
-    "circle": new Circle(canvas, {r: 50, stroke: "black", fill: "white"})
-  }
+    rect: new Rect(canvas, { w: 100, h: 100, style: "black" }),
+    circle: new Circle(canvas, { r: 50, stroke: "black", fill: "white" }),
+  };
 }
 
 export default class Canvas {
@@ -17,25 +20,32 @@ export default class Canvas {
   drawOptions: DrawOptions;
   _active: keyof DrawOptions;
   mousePosition: Coordinates;
+  history: (() => void)[];
+  _historyStep: number;
 
   constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
     this.ctx = ctx;
     this.drawOptions = createDrawOptions(this);
     this._active = "circle";
     this.element = canvas;
+    this.history = [];
+    this._historyStep = 0;
     this.mousePosition = {
-        x:0,
-        y:0
-    }
+      x: 0,
+      y: 0,
+    };
   }
 
-  get elementPosition (): Coordinates {
-      return getElementPosition(this.element)
+  private get elementPosition(): Coordinates {
+    return getElementPosition(this.element);
   }
 
   get clickPosition(): Coordinates {
-      const {dx: x, dy: y} = getDistanceFromTo(this.elementPosition, this.mousePosition)
-      return {x, y};
+    const { dx: x, dy: y } = getDistanceFromTo(
+      this.elementPosition,
+      this.mousePosition
+    );
+    return { x, y };
   }
 
   get active(): keyof DrawOptions {
@@ -48,35 +58,67 @@ export default class Canvas {
 
     if (Object.prototype.hasOwnProperty.call(this.drawOptions, value)) {
       this.drawOptions[value].select();
-      this._active = value
+      this._active = value;
     }
 
     this.setButtonActivity(value);
   }
 
-  updatePosition(e: MouseEvent):void {
-    this.mousePosition  = {
-        x: e.clientX,
-        y: e.clientY
-    }
+  get historyStep():number {
+    return this._historyStep;
   }
 
-  setButtonActivity(key: keyof DrawOptions, active = true):void {
+  set historyStep(step: number) {
+    this._historyStep = step;
+    this.drawHistory();
+  }
+
+  updatePosition(e: MouseEvent): void {
+    this.mousePosition = {
+      x: e.clientX,
+      y: e.clientY,
+    };
+  }
+
+  addToHistory(drawFunction: () => void): void {
+    if (this.historyStep < 0) {
+      this.history = this.history.slice(this.historyStep);
+      this.historyStep = 0;
+    }
+    this.history.push(drawFunction);
+  }
+
+  revert(): void {
+    this.historyStep -= 1;
+  }
+
+  restore():void {
+    this.historyStep += 1;
+  }
+
+  init(): string {
+    this.drawOptions[this.active].select();
+    this.active = this._active;
+    return this._active;
+  }
+
+  private setButtonActivity(key: keyof DrawOptions, active = true): void {
     const button = document.querySelector(`[data-drawable=${key}]`);
 
-    if(button){
+    if (button) {
       button.classList[active ? "add" : "remove"]("active");
     }
   }
 
-  draw(): void {
-    const drawable = this.drawOptions[this.active];
-    drawable.draw(this.clickPosition);
+  private drawHistory(): void {
+    this.clear();
+
+    const historyState = this.history.slice(0, this.historyStep);
+    historyState.forEach((element) => element());
   }
 
-  init():string {
-    this.drawOptions[this.active].select();
-    this.active = this._active;
-    return this._active
+  private clear(): void {
+    const { width, height } = this.element.getBoundingClientRect();
+    this.ctx.clearRect(0, 0, width, height);
   }
 }
