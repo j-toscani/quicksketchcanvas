3e759d347487dd0dcffeb1f61c15e0f6dec35121
Julian Toscani <toscani@cekom.de>
2021-08-01 23:09:08 +0200

    add logic to select movedrawable

--- src/abstracts/MoveDrawable.ts
+++ src/abstracts/MoveDrawable.ts
@@ -5,13 +5,30 @@ export abstract class MoveDrawable<T> extends DrawElement<T> implements Drawable
     constructor(canvas: Canvas, data: T) {
         super(canvas, data);
     }
+
     handler = (e: MouseEvent): void => { 
-       console.log(e);
+        this.canvas.updatePosition(e);
+        const position = {...this.canvas.clickPosition};
+
+        this.draw(position);
+    }
+    activate():void {
+        const canvas = this.canvas.element;
+        canvas.addEventListener("mousemove", this.handler);
+
+        const deactivate = () => {
+            canvas.removeEventListener("mousemove", this.handler);
+            canvas.removeEventListener("mouseup", deactivate);
+            canvas.removeEventListener("mouseleave", deactivate);
+        }
+
+        canvas.addEventListener("mouseup", deactivate)
+        canvas.addEventListener("mouseleave", deactivate)
     }
     select(): void {
-        this.canvas.element.addEventListener("mousemove", this.handler)
+        this.canvas.element.addEventListener("mousedown", this.activate)
     }
     deselect(): void {
-        this.canvas.element.removeEventListener("mousemove", this.handler)
+        this.canvas.element.removeEventListener("mousedown", this.activate)
     }
 }
