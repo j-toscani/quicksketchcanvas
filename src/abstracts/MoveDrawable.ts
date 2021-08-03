a803c203da6776d86fdee8d4086752ada506e669
Julian Toscani <toscani@cekom.de>
2021-08-03 22:25:04 +0200

    add history to MoveDrawables and refactor

--- src/abstracts/MoveDrawable.ts
+++ src/abstracts/MoveDrawable.ts
@@ -2,39 +2,62 @@ import Canvas from "../lib/Canvas.js";
 import { DrawElement } from "./DrawElement.js";
 
 export abstract class MoveDrawable<T> extends DrawElement<T> implements Drawable {
-    from: Coordinates | null
+    points: Coordinates[];
 
     constructor(canvas: Canvas, data: T) {
         super(canvas, data);
-        this.from = null;
+        this.points = [];
     }
+
+    abstract start(e:MouseEvent):void
+    abstract stop(e:MouseEvent):void
+    abstract setupStyle():void
 
     handler = (e: MouseEvent): void => { 
         this.canvas.updatePosition(e);
         const position = {...this.canvas.clickPosition};
 
         this.draw(position);
+        this.trackJourney(position);
     }
 
-    abstract start(e:MouseEvent):void
-    abstract stop(e:MouseEvent):void
+    trackJourney(coordinates: Coordinates):void {
+        this.points.push({...coordinates})
+    }
 
     activate = (e : MouseEvent):void => {
-        const canvas = this.canvas.element;
+        this.canvas.updatePosition(e)
+        this.points = [{...this.canvas.clickPosition}];
         
+        this.addHandler(this.canvas.element)
         this.start(e);
-        
+    }
+    
+    addHandler(canvas: HTMLCanvasElement) {
         canvas.addEventListener("mousemove", this.handler);
         canvas.addEventListener("mouseup", this.deactivate)
         canvas.addEventListener("mouseleave", this.deactivate)
     }
+
     deactivate = (e: MouseEvent):void => {
-        const canvas = this.canvas.element;
+        this.removeHandler(this.canvas.element);   
         this.stop(e)
+        const points = this.points.map((point) => ({...point}));
+        const drawMovement = () => {
+            this.canvas.ctx.beginPath();
+            points.forEach(point => this.draw(point));
+            this.canvas.ctx.closePath();
+        }
+
+        this.canvas.addToHistory(drawMovement);
+    }
+
+    removeHandler(canvas: HTMLCanvasElement) {
         canvas.removeEventListener("mousemove", this.handler);
         canvas.removeEventListener("mouseup", this.deactivate);
-        canvas.removeEventListener("mouseleave", this.deactivate);        
+        canvas.removeEventListener("mouseleave", this.deactivate);
     }
+
     select(): void {
         this.canvas.element.addEventListener("mousedown", this.activate)
     }
