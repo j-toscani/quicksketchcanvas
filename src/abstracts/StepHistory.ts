b215ee98385d4834bfd6cfc15808731b0694930b
Julian Toscani <toscani@cekom.de>
2021-11-28 16:18:01 +0100

    format

--- src/abstracts/StepHistory.ts
+++ src/abstracts/StepHistory.ts
@@ -1,42 +1,42 @@
 export default abstract class StepHistory<T> {
-    history: T[];
-    _historyStep: number;
+  history: T[];
+  _historyStep: number;
 
-    constructor() {
-        this.history = []
-        this._historyStep = 0
+  constructor() {
+    this.history = [];
+    this._historyStep = 0;
+  }
+
+  get historyStep(): number {
+    return this._historyStep;
+  }
+
+  set historyStep(step: number) {
+    this._historyStep = Math.min(Math.max(step, this.history.length * -1), 0);
+    this.onHistoryUpdate();
+  }
+
+  abstract onHistoryUpdate(): void;
+
+  addToHistory(step: T): void {
+    if (this.historyStep < 0) {
+      this.history = this.history.slice(0, this.historyStep);
     }
+    this.history.push(step);
+    this.historyStep = 0;
+  }
 
-    get historyStep(): number {
-      return this._historyStep;
-    }
-  
-    set historyStep(step: number) {
-      this._historyStep = Math.min(Math.max(step, this.history.length * -1), 0);
-      this.onHistoryUpdate();
-    }
+  revert(): void {
+    this.historyStep -= 1;
+  }
 
-    abstract onHistoryUpdate():void
+  restore(): void {
+    this.historyStep += 1;
+  }
 
-    addToHistory(step: T): void {
-        if (this.historyStep < 0) {
-          this.history = this.history.slice(0, this.historyStep);
-        }
-        this.history.push(step);
-        this.historyStep = 0;
-      }
-    
-      revert(): void {
-        this.historyStep -= 1;
-      }
-    
-      restore(): void {
-        this.historyStep += 1;
-      }
-    
-      getHistoryState(): T[] {
-        return this.historyStep
-          ? this.history.slice(0, this.historyStep)
-          : this.history.slice(0);
-      }
+  getHistoryState(): T[] {
+    return this.historyStep
+      ? this.history.slice(0, this.historyStep)
+      : this.history.slice(0);
+  }
 }
