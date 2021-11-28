dbe9d18962bc5c66e6b8f17641c60981b17c6d8c
Julian Toscani <toscani@cekom.de>
2021-11-28 11:28:35 +0100

    add stroke to rectangles

--- src/drawables/Rect.ts
+++ src/drawables/Rect.ts
@@ -15,11 +15,17 @@ export default class Rect extends ClickDrawable<RectData> {
 
   setupStyle() {
     this.canvas.ctx.fillStyle = this.data.fill;
+    this.canvas.ctx.strokeStyle = this.data.stroke;
   }
 
   draw(position: Coordinates): void {
     const { x, y, w, h } = { ...position, ...this.data };
+
+    this.canvas.ctx.beginPath();
     this.setupStyle();
-    this.canvas.ctx.fillRect(x - w / 2, y - h / 2, w, h);
+
+    this.canvas.ctx.rect(x - w / 2, y - h / 2, w, h);
+    this.canvas.ctx.stroke();
+    this.canvas.ctx.fill();
   }
 }
