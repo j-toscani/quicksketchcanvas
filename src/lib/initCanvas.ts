2784ec394d21ad059ad4337c615ff4feb7cac26c
Julian Toscani <toscani@cekom.de>
2021-11-28 18:25:42 +0100

    update canvas initialisation

--- src/lib/initCanvas.ts
+++ src/lib/initCanvas.ts
@@ -1,33 +1,10 @@
 import createCanvas from "./createCanvas";
 import Canvas from "../Canvas";
 import initDrawableButtons from "./initDrawableButtons";
-import { DrawElement } from "../abstracts/DrawElement";
-import { Drawable } from "../types";
 
-type CanvasInfos = {
-  canvas: Canvas;
-  active: Drawable;
-  available: string[];
-};
-
-export default function initCanvas(query: string): CanvasInfos {
+export default function initCanvas(query: string): Canvas {
   const canvas = createCanvas(query);
-  canvas.init();
-  const available = initDrawableButtons(canvas);
+  initDrawableButtons(canvas);
 
-  initRevertRestore(canvas);
-
-  return {
-    canvas: canvas,
-    active: canvas.active,
-    available,
-  };
-}
-
-function initRevertRestore(canvas: Canvas) {
-  const revert = document.querySelector("#revert");
-  const restore = document.querySelector("#restore");
-
-  revert?.addEventListener("click", () => canvas.revert());
-  restore?.addEventListener("click", () => canvas.restore());
+  return canvas;
 }
