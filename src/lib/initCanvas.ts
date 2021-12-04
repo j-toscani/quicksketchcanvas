5a587b1c1ddad195a1e84f9085df3ecdfb8ccbdf
Julian Toscani <julian_toscani@gmx.de>
2021-12-04 12:56:50 +0100

    add style changer

--- src/lib/initCanvas.ts
+++ src/lib/initCanvas.ts
@@ -1,10 +1,12 @@
 import createCanvas from "./createCanvas";
 import Canvas from "../Canvas";
 import initDrawableButtons from "./initDrawableButtons";
+import activateStyleSelect from "./activateStyleSelect";
 
 export default function initCanvas(query: string): Canvas {
   const canvas = createCanvas(query);
   initDrawableButtons(canvas);
+  activateStyleSelect(canvas);
 
   return canvas;
 }
